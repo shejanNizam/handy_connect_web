57681cf6abcabbdf4d4d4fe35f9cbe483c430942
nizam <nizam.shejan11@gmail.com>
2026-03-10 14:56:10 +0600

    change the dynamic category page

--- src/app/(customerDashboard)/user-dashboard/page.tsx
+++ src/app/(customerDashboard)/user-dashboard/page.tsx
@@ -110,7 +110,7 @@ export default function UserDashboard() {
           Categories
         </h3>
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
-          {categories.map((category) => (
+          {categories?.map((category) => (
             <Link
               key={category.name}
               href={category.href}
